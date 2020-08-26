3198cb1e94638cc33bcb2d04e6008e08ee8d8cb8
juan.wong <juan.ramirez@wolox.co>
2020-08-26 10:46:44 -0500

    Add custom query in BookRepository, Add test for allFilters query, Add controller default call

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -2,6 +2,7 @@ package wolox.training.repositories;
 
 import java.util.List;
 import java.util.Optional;
+import javax.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -24,4 +25,22 @@ public interface BookRepository extends CrudRepository<Book, Long> {
             + " AND (:year is null or b.year = :year)")
     public Optional<List<Book>> findByPublisherGenreAndYearCustom(@Param("publisher") String publisher,
             @Param("genre") String genre, @Param("year") String year);
+
+    @Query("SELECT b "
+            + " FROM Book b "
+            + " WHERE (:genre = '' or :genre is null or b.genre = :genre)"
+            + " AND (:author = '' or :author is null or b.author = :author)"
+            + " AND (:image = '' or :image is null or b.image = :image)"
+            + " AND (:title = '' or :title is null or b.title = :title)"
+            + " AND (:subtitle = '' or :subtitle is null or b.subtitle = :subtitle)"
+            + " AND (:publisher = '' or :publisher is null or b.publisher = :publisher)"
+            + " AND (:year = '' or :year is null or b.year = :year)"
+            + " AND (:pages is null or CAST(:pages as int) <= 0 or CAST(b.pages as int) = :pages)"
+            + " AND (:isbn = '' or :isbn is null or b.isbn = :isbn)")
+    public Optional<List<Book>> findByAllFields(@Param("genre") String genre,
+            @Param("author") String author, @Param("image") String image,
+            @Param("title") String title, @Param("subtitle") String subtitle,
+            @Param("publisher") String publisher, @Param("year") String year,
+            @Param("pages") Integer pages, @Param("isbn") String isbn);
+
 }
