42a7a62cb5f5d65122f67d0a82e4f9044a0e2fa2
juan.wong <juan.ramirez@wolox.co>
2020-08-26 16:28:59 -0500

    Add pagging to find method, add params in controller to paging and sort, add mehod in BookService

--- src/main/java/wolox/training/services/BookService.java
+++ src/main/java/wolox/training/services/BookService.java
@@ -1,8 +1,12 @@
 package wolox.training.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import wolox.training.models.Book;
 import wolox.training.models.DTO.BookDTO;
@@ -29,5 +33,18 @@ public class BookService {
     public Book getBookModelFromDTO(BookDTO bookDTO){
         Book book = new Book(bookDTO);
         return book;
+    }
+
+    public List<Book> findByAllFieldsPaging( String genre, String author, String image,
+            String title, String subtitle, String publisher, String year, Integer pages,
+            String isbn, String startYear, String endYear, Integer page, Integer size,
+            String sortBy, String order){
+
+        Pageable pageable = PageRequest.of(page, size,
+                order.equalsIgnoreCase("asc") ? Sort.by(sortBy).ascending() :
+                        Sort.by(sortBy).descending());
+
+        return bookRepository.findByAllFieldsPaging(genre, author, image, title, subtitle, publisher,
+                year, pages, isbn, startYear, endYear, pageable).orElse(new ArrayList<Book>());
     }
 }
